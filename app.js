9a0b90c94593813a5e2ac43df97350f0737318dc
Carlos Amado <exchangeside@gmail.com>
2022-07-02 08:09:05 -0400

    Finished

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const app = express();
 
+app.set("views", __dirname + "/views");
+app.set("view engine", "hbs");
 app.use(express.static('public'));
 
 app.get('/', (request, response, next) => {
-  response.sendFile(__dirname + '/views/home-page.html')
+  response.render('home')
   console.log(__dirname)
 });
 
 app.get('/about', (request, response, next) => {
-  response.sendFile(__dirname + '/views/about-page.html')
+  response.render('about')
 });
 
 app.get('/inventions', (request, response, next) => {
-  response.sendFile(__dirname + '/views/inventions-page.html')
+  response.render('inventions')
 });
 
 app.listen(3000, () => console.log('My first app listening on port 3000! '));
